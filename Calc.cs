1da6553e3e9cda127e021cc470527148914df241
Adrian Szarnowski <nynu88888888@gmail.com>
2020-04-06 12:07:15 +0200

    Initial commit

---
+++ Calc.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Calc
+    {
+        private decimal Result = 0;
+        public string GetResult { 
+            get 
+            {
+                string stringValue = Result.ToString(CultureInfo.CreateSpecificCulture("en-GB"));
+                var decimalPlaces = 0;
+                if (stringValue.Contains("."))
+                {
+                    decimalPlaces = stringValue.Length - stringValue.IndexOf(".") - 1;
+                }
+                var format = new NumberFormatInfo { NumberGroupSeparator = " " };
+                string output;
+                if (Result.ToString().Length > 13)
+                {
+                    output = Result.ToString("E", format);
+                }
+                else
+                {
+                    output = Result.ToString($"F{decimalPlaces}", format);
+                }
+                return output;
+            } 
+            private set { } }
+
+        private readonly List<char> Operators = new List<char>()
+        {
+            '+', '-', '*', '/', '^', '(', ')'
+        };
+
+        private readonly List<string> AdjecentOperators = new List<string>()
+        {
+            "*-", "(-", "-(", "+(", "*(", "/(", "((", ")+", ")-", ")*", ")/", "^-", "^(", ")^"
+        };
+
+        public bool CanInsertOperator(string op1, string op2)
+        {
+            if(int.TryParse(op1, out int _))
+            {
+                if(op2 == "(")
+                {
+                    return false;
+                }
+            }
+
+            string combination = op1 + op2;
+            if (AdjecentOperators.Contains(combination))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Reset()
+        {
+            Result = 0;
+        }
+
+        public void EvaluateExpression(string expression)
+        {
+            Stack<decimal> values = new Stack<decimal>();
+            Stack<string> operators = new Stack<string>();
+            if (expression.StartsWith("-"))
+            {
+                //algorithm can only evaluate "a operator b" expressions so we're adding 0 to have 0-stuff
+                expression = "0" + expression;
+            }
+
+            int i = 0;
+            while(i < expression.Length)
+            {
+                string nextCharacter = GetNextExpressionCharacter(expression, i);
+                if(nextCharacter.Length >= decimal.MaxValue.ToString().Length)
+                {
+                    throw new ArithmeticException();
+                }
+
+                if(decimal.TryParse(nextCharacter, NumberStyles.Any, CultureInfo.CreateSpecificCulture("en-GB"), out var num))
+                {
+                    values.Push(num);
+                }
+                else
+                {
+                    if(nextCharacter == "(")
+                    {
+                        operators.Push(nextCharacter);
+                    }
+                    else if(nextCharacter == ")")
+                    {
+                        while (operators.Count > 0 && operators.Peek() != "(")
+                        {
+                            var op = operators.Pop();
+                            var val2 = values.Pop();
+                            var val1 = values.Pop();
+                            values.Push(ApplyOperator(val1, val2, op));
+                        }
+
+                        operators.Pop();
+                    }
+                    else if (Operators.Contains(nextCharacter[0]))
+                    {
+                        while(operators.Count > 0 && Precedence(operators.Peek()) >= Precedence(nextCharacter))
+                        {
+                            var op = operators.Pop();
+                            var val2 = values.Pop();
+                            var val1 = values.Pop();
+                            values.Push(ApplyOperator(val1, val2, op));
+                        }
+
+                        operators.Push(nextCharacter);
+                    }
+                }
+
+                i += nextCharacter.Length;
+            }
+
+            if(operators.Count + 1 != values.Count)
+            {
+                //expression isn't complete, we don't want to do anything
+                throw new Exception();
+            }
+
+            while (operators.Count > 0)
+            {
+                var op = operators.Pop();
+                var val2 = values.Pop();
+                var val1 = values.Pop();
+                values.Push(ApplyOperator(val1, val2, op));
+            }
+
+            Result = values.Pop();
+        }
+
+        private decimal ApplyOperator(decimal val1, decimal val2, string op)
+        {
+            switch (op)
+            {
+                case "+": return val1 + val2;
+                case "-": return val1 - val2;
+                case "*": return val1 * val2;
+                case "/": return val1 / val2;
+                case "^": return (decimal)Math.Pow((double)val1, (double)val2);
+                default: throw new Exception("Invalid operator");
+            }
+        }
+
+        private string GetNextExpressionCharacter(string expression, int startIndex)
+        {
+            var currentCharacter = expression[startIndex];
+            if (Operators.Contains(currentCharacter) && (currentCharacter != '-' || (currentCharacter == '-' && (!Operators.Contains(expression[startIndex-1]) || expression[startIndex-1] == ')'))))
+            {
+                return currentCharacter.ToString();
+            }
+            else
+            {
+                string result = expression[startIndex++].ToString();
+                while (startIndex < expression.Length && (!Operators.Contains(expression[startIndex]) || Operators.Contains(expression[startIndex]) && expression[startIndex-1] == 'E')) 
+                {
+                    result += expression[startIndex];
+                    startIndex++;
+                }
+
+                return result;
+            }
+        }
+
+        private int Precedence(string op)
+        {
+            if (op == "+" || op == "-")
+                return 1;
+            if (op == "*" || op == "/")
+                return 2;
+            if (op == "^")
+                return 3;
+            return 0;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Stopwatch stopwatch = new Stopwatch();
+        private Calc calculator = new Calc();
+        private bool errorRaised = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+            EventManager.RegisterClassHandler(typeof(Button), Button.ClickEvent, new RoutedEventHandler(OnButtonClick));
+        }
+
+        private void DeleteCharacter()
+        {
+            if (errorRaised)
+            {
+                ClearError();
+            }
+            var text = expressionText.Text;
+            if(text.Length > 0)
+            {
+                expressionText.Text = text.Substring(0, text.Length - 1);
+                UpdateOutput();
+            }
+            else
+            {
+                resultText.Text = "";
+            }
+
+            ScrollToEnd();
+        }
+
+        private void DeleteEverything()
+        {
+            expressionText.Text = "";
+            resultText.Text = "";
+            ScrollToEnd();
+        }
+
+        private void AddCharacter(string character)
+        {
+            expressionText.AppendText(character);
+            ScrollToEnd();
+        }
+
+        private void ScrollToEnd()
+        {
+            expressionText.Select(expressionText.Text.Length, 0);
+            expressionText.Focus();
+        }
+
+        private void AddOperator(string op)
+        {
+            if (op == "=" && !errorRaised)
+            {
+                expressionText.Text = resultText.Text;
+            }
+            else if (expressionText.Text.Length == 0)
+            {
+                if (op == "-" || op == "(")
+                {
+                    AddCharacter(op);
+                }
+            }
+            else if (op == ")")
+            {
+                if (expressionText.Text.Count(ch => ch == '(') > expressionText.Text.Count(ch => ch == ')'))
+                {
+                    if(GetLastCharacter() != "(")
+                    {
+                        AddCharacter(")");
+                    }
+                }
+            }
+            else if (int.TryParse(GetLastCharacter(), out int _) && op != "(")
+            {
+                AddCharacter(op);
+            }
+            else if (calculator.CanInsertOperator(GetLastCharacter(), op))
+            {
+                AddCharacter(op);
+            }
+            else {
+                if(op != "(")
+                {
+                    DeleteCharacter();
+                    if(expressionText.Text.Length == 0)
+                    {
+                        AddOperator(op);
+                        return;
+                    }
+                    if(GetLastCharacter() != "*")
+                    {
+                        AddCharacter(op);
+                    }
+                }
+            }
+        }
+
+        private void UpdateOutput()
+        {
+            try
+            {
+                calculator.EvaluateExpression(expressionText.Text);
+                resultText.Text = calculator.GetResult;
+            }
+            catch (DivideByZeroException)
+            {
+                RaiseError("Div by 0");
+            }
+            catch (OverflowException)
+            {
+                RaiseError("Value too big");
+            }
+            catch (ArithmeticException)
+            {
+                RaiseError("Number too long");
+            }
+            catch (Exception)
+            {
+                //empty on purpose
+                //this means that the evaluation is invalid because expression is not yet complete
+            }
+        }
+
+        private void RaiseError(string err)
+        {
+            resultText.Text = err;
+            errorRaised = true;
+            resultText.Foreground = Brushes.Red;
+        }
+
+        private void ClearError()
+        {
+            errorRaised = false;
+            resultText.Foreground = Brushes.Gray;
+        }
+
+        private string GetLastCharacter()
+        {
+            return expressionText.Text[expressionText.Text.Length - 1].ToString();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button src = (Button)sender;
+            if (int.TryParse(src.Content.ToString(), out int _))
+            {
+                AddCharacter(src.Content.ToString());
+            }
+            else
+            {
+                AddOperator(src.Content.ToString());
+            }
+
+            UpdateOutput();
+        }
+
+        private void DelButton_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            stopwatch.Start();
+            e.Handled = true;
+        }
+
+        private void DelButton_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            stopwatch.Stop();
+            if(stopwatch.ElapsedMilliseconds > 500)
+            {
+                DeleteEverything();
+            }
+            else
+            {
+                DeleteCharacter();
+            }
+
+            stopwatch.Reset();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(((int)e.Key >= 34 && (int)e.Key <= 43) || ((int)e.Key >= 74 && (int)e.Key <= 83))
+            {
+                AddCharacter(e.Key.ToString().Substring(e.Key.ToString().Length - 1));
+            }
+
+            switch(e.Key)
+            {
+                case Key.Back: DeleteCharacter(); break;
+                case Key.Enter: AddOperator("="); break;
+                case Key.OemPlus:
+                case Key.Add: AddOperator("+"); break;
+                case Key.Oem2:
+                case Key.Divide: AddOperator("/"); break;
+                case Key.OemMinus:
+                case Key.Subtract: AddOperator("-"); break;
+                case Key.Multiply: AddOperator("*"); break;
+                case Key.OemPeriod: AddOperator("."); break;
+                default: break;
+            }
+
+            UpdateOutput();
+        }
+    }
+}
